48a913c7306b264143c153fd24952fb1fdda5714
ph4rr3ll <pharrell@yandex.ru>
2014-07-11 12:05:47 +0300

    Футер

---
+++ footer.php
@@ -0,0 +1,11 @@
+			<div id="footer">
+				<p class="p_foot"><a href="/">Главная</a>  |  
+				<a href="/study/">Учебный материал</a>   |    
+				<a href="/teach/">От преподавателей</a>  |   
+				<a href="/galery/">Галерея</a>  |    
+				<a href="/chat/">Чат</a>   |    
+				<a href="/about.php">Помощь</a><br>
+				&copy; Green Code
+				<br>2013
+				</p>
+			</div>  
