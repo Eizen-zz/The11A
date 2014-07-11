9f702f9b5d67fa7849c47dcfe2b3c639e9a4d405
ph4rr3ll <pharrell@yandex.ru>
2014-07-11 12:04:40 +0300

    Соединение с базой данных

---
+++ connect.php
@@ -0,0 +1,9 @@
+﻿<?
+	$host='';
+	$user='';
+	$pass='';
+	$db='';
+	$connect = mysql_connect($host, $user, $pass);
+	if (!$connect || !mysql_select_db($db, $connect))
+		exit(mysql_error());
+	mysql_set_charset('utf8', $connect);
