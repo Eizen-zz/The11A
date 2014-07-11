47400cf461222da8c21104ba3ff0c584a88e9abd
ph4rr3ll <pharrell@yandex.ru>
2014-07-11 12:10:19 +0300

    Для кнопок

---
+++ table.php
@@ -0,0 +1,9 @@
+<link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+<div class="menu-table">
+  <a class="link" href="/">Главная</a>
+  <a class="link" href="/study/">Учебный материал</a>
+  <a class="link" href="/teach/">От преподавателей</a>
+  <a class="link" href="/galery/">Галерея</a>
+  <a class="link" href="/chat/">Чат</a>
+  <a class="link" href="/about.php">Помощь</a>
+</div>
